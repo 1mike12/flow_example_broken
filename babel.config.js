daad54fa22cd28dc046d58e451e9c699d65142e3
Michael Qin <1mike12@gmail.com>
2022-04-08 09:16:57 -0500

    get rid of additional transforms

--- babel.config.js
+++ babel.config.js
@@ -5,8 +5,8 @@ module.exports = {
     },
     plugins: [
         ["@babel/plugin-proposal-decorators", {legacy: true,}],
-        ["@babel/plugin-transform-flow-strip-types"],
-        ["@babel/plugin-proposal-class-properties", {"loose": false}],
+        // ["@babel/plugin-transform-flow-strip-types"],
+        // ["@babel/plugin-proposal-class-properties", {"loose": false}],
         ["@babel/plugin-proposal-optional-catch-binding"],
     ],
 }
